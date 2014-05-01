aed0423bf71d085d393ddf01158af08921f3c0ec
KOBAYASI, Hiroaki <buribullet@gmail.com>
2014-05-01 21:44:49 +0900

    Tests for read_seplist and read_tcl_quote

--- listReader_test.ml
+++ listReader_test.ml
@@ -16,9 +16,9 @@ let string_of_optlist l' =
   | Some l -> List.to_string ~f:ident l
   | None -> "(none)"
 
-let cc_sp   = CC.of_string "-0x09 -0x20"
-
-let cc_name = CC.of_string "A-Z a-z _ 0-9"
+let cc_sp    = CC.of_string "-0x09 -0x20"
+let cc_name  = CC.of_string "A-Z a-z _ 0-9"
+let cc_comma = CC.of_string ","
 
 let () = run_test_tt_main
   ("listReader">:::
@@ -31,4 +31,22 @@ let () = run_test_tt_main
 		 ~body:(SM.tab ~matching:(SM.many cc_name))
 		 (SC.init "foo bar baz"))
 	       &&>> Ring.to_list)
-	  )])
+	  )]
+  @ ["read_seplist">::
+	  (fun _ ->
+	    assert_equal ~printer:string_of_optlist
+	      (Some ["1"; ","; "2"; ","; "3"])
+	      ((M.read_seplist
+		 ~sep:(SM.tab ~matching:(SM.many cc_comma))
+		 ~body:(SM.tab ~matching:(SM.many cc_name))
+		 (SC.init "1,2,3"))
+	       &&>> Ring.to_list)
+	  )]
+
+  @ ["read_tcl_quote">::
+	  (fun _ ->
+	    assert_equal 
+	      "{foo bar{baz}}"
+	      (M.read_tcl_quote (SC.init "{foo bar{baz}} bang"))
+	  )]
+  )
